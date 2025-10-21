464b9e2dbded3ce0c8e6525355f7de1c058b101c
mahmud014 <anik955720@gmail.com>
2025-10-21 22:07:47 +0600

    Project setup install react router, icons, tailwid, daisyUI, google font, motion etc.

---
+++ src/Pages/AddSkill.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddSkill = () => {
+  return (
+    <div>
+      <h1>Add skill</h1>
+    </div>
+  );
+};
+
+export default AddSkill;
---
+++ src/Roots/Root.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import { Outlet } from "react-router";
+import Footer from "../Components/Footer";
+
+const Root = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <Navbar />
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Root;
